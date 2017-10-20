4e4bf0497a56d27d79b67d56260920325246cc50
Misael Pereyra <misaelpermor@gmail.com>
2017-10-19 23:26:53 -0500

    Revert "Segunda linea"
    re

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
 </head>
 <body>
 	<p>Bienvenido a la web de prueba.</p>
+<<<<<<< HEAD
 	<p>Esta es la segunda linea que se agrego por separado</p>
 	<p>vamos a ver si me regresa a a otra lina</p>
 
 	<p>o no</p>
+=======
+>>>>>>> parent of 7405f31... Segunda linea
 </body>
 </html>
